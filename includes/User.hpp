e9a6090001976a305f5083cc24e94e8a10747f7b
jode-vri <jode-vri@student.42.fr>
2022-09-11 04:13:59 +0000

    send message to user

--- includes/User.hpp
+++ includes/User.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <vector>
 
 #include "BasicConnection.hpp"
 
@@ -10,13 +11,24 @@ class User : public TCP::BasicConnection {
         const std::string       &nickname() const;
         void					setNickname(const std::string &nickname);
 
+        const std::string       &username() const;
+        void					setUsername(const std::string &username);
+
+        const std::string       &realname() const;
+        void					setRealname(const std::string &realname);
+
         int                     state() const;
         void					setState(int state);
 
         bool                    isRegistered() const;
 
+        std::string             getResponse(User &u, int code, std::vector<std::string> args);
+        int                     reply(User &u, int code, std::vector<std::string> args);
+
     private:
         std::string             _nickname;
+        std::string             _username;
+        std::string             _realname;
         bool                    _registered;
         int                     _state;
 
--- srcs/User.cpp
+++ srcs/User.cpp
@@ -1,4 +1,5 @@
 #include "User.hpp"
+#include <iostream>
 
 User::User(TCP::TCPSocket *socket) : 
 	BasicConnection(socket),
@@ -10,10 +11,40 @@ User::User(TCP::TCPSocket *socket) :
 User::~User() {}
 
 const std::string		&User::nickname() const { return (_nickname); }
-
 void 					User::setNickname(const std::string &nickname) { _nickname = nickname; }
+
+
+const std::string		&User::username() const { return (_username); }
+void 					User::setUsername(const std::string &username) { _username = username; }
+
+const std::string		&User::realname() const { return (_realname); }
+void 					User::setRealname(const std::string &realname) { _realname = realname; }
+
 
 bool                    User::isRegistered() const { return (_registered); }
 
 int                     User::state() const { return (_state); }
 void					User::setState(int state) { _state = state; }
+
+
+int                     User::reply(User &u, int code, std::vector<std::string> args) {
+	u.write(getResponse(u, code, args));
+	return (0);
+}
+
+std::string             User::getResponse(User &u, int code, std::vector<std::string> args) {
+	std::string target;
+	if (u.state() == 0 || u.state() == 1)
+		target = "*";
+	else
+		target = u.nickname();
+	target += " ";
+	switch (code) {
+		case 431:
+			return target + ":No nickname given";
+		case 461:
+			return target + ": " + args[0] + " :Not enough parameters";
+		default:
+			return std::string(args[0]);
+	}
+}
