591d800ea5fd9e8aef9c420643655fae8c98a36f
jode-vri <jode-vri@student.42.fr>
2022-09-16 20:02:05 +0000

    update pass command

--- srcs/cmdPASS.cpp
+++ srcs/cmdPASS.cpp
@@ -2,12 +2,12 @@
 
 int		IrcServer::PASS(User &u, Message msg) {
 
-	if (u.state() != 0) {
+	if (u.state() == 2) {
 		u.reply(u, ERR_ALREADYREGISTRED(u.nickname()));
 		return 0;
 	}
 	if (msg.args().size() != 1)
-		return u.reply(u, ERR_NEEDMOREPARAMS(u.nickname(), msg.args()[0]));
+		return u.reply(u, ERR_NEEDMOREPARAMS(u.nickname(), msg.command()));
 	if (msg.args()[0] != _password)
 		return u.reply(u, ERR_PASSWDMISMATCH(u.nickname()));
 	u.setState(1);
