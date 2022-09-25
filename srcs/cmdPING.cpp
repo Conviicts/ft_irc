5ea3bef6a18850c88d9a02f703191760cccbe09a
jode-vri <jode-vri@student.42.fr>
2022-09-25 03:01:17 +0000

    ping command

--- srcs/cmdPING.cpp
+++ srcs/cmdPING.cpp
@@ -1,9 +1,8 @@
 #include "IrcServer.hpp"
 
 int		IrcServer::PING(User &u, Message msg) {
-	(void)u;
-	(void)msg;
-
-	std::cout << "TODO: PING" << std::endl; 
+	if (msg.args().size() == 0)
+		return u.reply(u, ERR_NOORIGIN(u.nickname()));
+	u.reply(u, RPL_PONG(u.nickname(), msg.args()[0]));
 	return (1);
 }
