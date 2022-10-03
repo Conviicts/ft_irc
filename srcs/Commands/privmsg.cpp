b90e694aad62c6358455f87864710b3f4e87eb88
jode-vri <jode-vri@student.42.fr>
2022-10-03 13:38:06 +0000

    privmsg & quit

--- srcs/Commands/privmsg.cpp
+++ srcs/Commands/privmsg.cpp
@@ -1,29 +1,35 @@
 #include "IrcServer.hpp"
-#include "ERR_RPL.hpp"
+#include "Replies.hpp"
+
+std::vector<std::string>    split(const std::string &str, char delim);
 
 int		IrcServer::PRIVMSG(User &u, Message msg) {
 	if (u.state() != 2)
 		return (u.reply(u, ERR_NOTREGISTERED(u.nickname())));
 	if (msg.args().size() < 2)
 		return (u.reply(u, ERR_NEEDMOREPARAMS(u.nickname(), "PRIVMSG")));
-	std::string target(msg.args()[0]);
 	std::string message;
 
 	for (std::vector<std::string>::const_iterator it = msg.args().begin() + 1; it != msg.args().end(); it++)
 		message.append(*it + " ");
 	message = message.at(0) == ':' ? message.substr(1) : message;
-	if (target.at(0) == '#') {
-		Channel *c = _network.getChannel(target);
-		if (!c)
-			return u.reply(u, ERR_NOSUCHCHANNEL(u.nickname(), msg.args()[0]));
-		if (!c->getUser(&u))
-			return u.reply(u, ERR_NOTONCHANNEL(u.nickname(), msg.args()[0]));
-		c->broadcast(&u, ":" + u.getPrefix() + " PRIVMSG " + target + " :" + message);
-	} else {
-		User *t = _network.getByNickname(target);
-		if (!t)
-			return u.reply(u, ERR_NOSUCHNICK(u.nickname(), msg.args()[0]));
-		t->write(":" + u.getPrefix() + " PRIVMSG " + target + " :" + message);
+	
+	std::vector<std::string> targets = split(msg.args()[0], ',');
+	for (std::vector<std::string>::const_iterator it = targets.begin(); it != targets.end(); it++) {
+		std::string target(*it);
+		if (target.at(0) == '#') {
+			Channel *c = _network.getChannel(target);
+			if (!c)
+				return u.reply(u, ERR_NOSUCHCHANNEL(u.nickname(), target));
+			if (!c->getUser(&u))
+				return u.reply(u, ERR_NOTONCHANNEL(u.nickname(), target));
+			c->broadcast(&u, ":" + u.getPrefix() + " PRIVMSG " + target + " :" + message);
+		} else {
+			User *t = _network.getByNickname(target);
+			if (!t)
+				return u.reply(u, ERR_NOSUCHNICK(u.nickname(), target));
+			t->write(":" + u.getPrefix() + " PRIVMSG " + target + " :" + message);
+		}
 	}
 	return (1);
 }
--- srcs/Commands/quit.cpp
+++ srcs/Commands/quit.cpp
@@ -5,7 +5,7 @@ int		IrcServer::QUIT(User &u, Message msg) {
 	if (u.state() != 2)
 		disconnect(u, "Client Quit", true);
 	else
-		disconnect(u, msg.args()[0], false);
+		disconnect(u, msg.args()[0].empty() ? u.nickname() : msg.args()[0], false);
 
 	return (1);
 }
